a6baaaeb06c61738bdaeea88c744869662a48639
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-05-04 16:11:36 +0200

    fix: deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-docker tag keboola/developer-portal-cli quay.io/keboola/developer-portal-cli-v2:$TRAVIS_TAG
+docker tag keboola/developer-portal-cli-v2 quay.io/keboola/developer-portal-cli-v2:$TRAVIS_TAG
+docker tag keboola/developer-portal-cli-v2 quay.io/keboola/developer-portal-cli-v2:latest
 docker push quay.io/keboola/developer-portal-cli-v2:$TRAVIS_TAG
+docker push quay.io/keboola/developer-portal-cli-v2:latest
