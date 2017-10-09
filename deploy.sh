a375c91a6fbe2d234714d588d870ce918ed8ba02
Martin Halamíček <martin@halamicek.cz>
2017-10-09 16:25:17 +0200

    deployment order changed

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-# Deploy to Quay.io
-docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-docker tag keboola/developer-portal-cli-v2 quay.io/keboola/developer-portal-cli-v2:$TRAVIS_TAG
-docker tag keboola/developer-portal-cli-v2 quay.io/keboola/developer-portal-cli-v2:latest
-docker push quay.io/keboola/developer-portal-cli-v2:$TRAVIS_TAG
-docker push quay.io/keboola/developer-portal-cli-v2:latest
-
 # Deploy to Dockerhub
 docker login -u="$DOCKERHUB_USERNAME" -p="$DOCKERHUB_PASSWORD" quay.io
 docker tag keboola/developer-portal-cli-v2 keboola/developer-portal-cli-v2:$TRAVIS_TAG
 docker tag keboola/developer-portal-cli-v2 qkeboola/developer-portal-cli-v2:latest
 docker push keboola/developer-portal-cli-v2:$TRAVIS_TAG
 docker push keboola/developer-portal-cli-v2:latest
+
+# Deploy to Quay.io
+docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
+docker tag keboola/developer-portal-cli-v2 quay.io/keboola/developer-portal-cli-v2:$TRAVIS_TAG
+docker tag keboola/developer-portal-cli-v2 quay.io/keboola/developer-portal-cli-v2:latest
+docker push quay.io/keboola/developer-portal-cli-v2:$TRAVIS_TAG
+docker push quay.io/keboola/developer-portal-cli-v2:latest
